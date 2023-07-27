9cd0cdbf9cbddd6a172414ca0fc7a62469b1a463
Philipp Bott <philipp.r.c.bott@gmail.com>
2023-07-27 11:10:54 +0200

    Initial commit

---
+++ src/main/java/com/example/traincartlocation/TrainCartLocationApplication.java
@@ -0,0 +1,13 @@
+package com.example.traincartlocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainCartLocationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainCartLocationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/traincartlocation/TrainCartLocationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.traincartlocation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainCartLocationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
