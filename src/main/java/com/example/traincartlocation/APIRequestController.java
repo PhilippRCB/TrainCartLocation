defd31dcbb46137c975f8f9eb19c5e5e8ef417e8
Philipp Bott <philipp.r.c.bott@gmail.com>
2023-07-31 16:41:48 +0200

    Optimized Endpoint String generation.

--- src/main/java/com/example/traincartlocation/APIRequestController.java
+++ src/main/java/com/example/traincartlocation/APIRequestController.java
@@ -8,11 +8,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class APIRequestController {
 
     @GetMapping("/station/{ril100}/train/{trainNumber}/waggon/{number}")
-    public String greeting(@PathVariable String number, @PathVariable String ril100, @PathVariable String trainNumber){
+    public String sectionRequest(@PathVariable int number, @PathVariable String ril100, @PathVariable int trainNumber){
         FileFinder finder = new FileFinder(ril100);
         XmlParser parser = new XmlParser(finder.getFileName());
-        if (parser.sectionList()[0].isEmpty()) return "Error!";
-        if (parser.sectionList()[1].isEmpty()) return "{ \"sections\": [\"%s\"] }".formatted(parser.sectionList()[0]);
-        return "{ \"sections\": [\"%s\", \"%s\"] }".formatted(parser.sectionList()[0], parser.sectionList()[1]);
+        String[] sections = parser.sectionList(trainNumber, number);
+        StringBuilder sectionString = new StringBuilder();
+        for (String section : sections) sectionString.append("\"%s\",".formatted(section));
+        sectionString.deleteCharAt(sectionString.length() - 1);
+        return "{ \"sections\": [%s] }".formatted(sectionString);
     }
 }
